a44537672da70b2b6991487eaaa6ab7c11054e38
kakau1401 <160244211+kakau1401@users.noreply.github.com>
2024-09-20 08:57:43 -0300

    Kickoff-nutricionista

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,18 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = massa / altura * altura;
+
+console.log (imc);
+
+console.log(paciente);
+console.log(tdaltura);
+console.log(altura);
